2928544c1111c0870792de58c7fe87d2f4268456
Oliver Morf <morfoli1@students.zhaw.ch>
2022-04-29 16:12:59 +0200

    added build and prod script, added cors

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { Request, Response } from 'express';
 import api from './api';
 import db from './db/db';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
@@ -10,6 +11,7 @@ db.init();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.use('/api', api);
 
 app.listen(PORT, () => {
